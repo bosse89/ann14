1e4681da79a86eede85ccc6a381011e6338048ca
bosse89 <bosse89@gmail.com>
2014-02-04 09:48:41 +0100

    lade fel fil tidigare
    Hade också problem med filnamn , detta gjorde jag senast igår innan jag
    gick från er.

---
+++ lab2/four_one_least_mean_sq.m
@@ -0,0 +1,21 @@
+x = [0:0.1:2*pi]';%inputs
+f=sin(2*x);%desired outputs
+units=5;
+for units=56:56
+makerbf %output: m and var
+Phi = calcPhi(x,m,var);
+w=Phi\f;
+y=Phi*w;
+rvmax=max(abs(f-y));
+%display(rvmax);
+if rvmax<=0.1
+    display(['0.1, ' num2str(units)]);
+end
+if rvmax<=0.01
+    display(['0.01, ' num2str(units)]);
+end
+if rvmax<=0.001
+    display(['0.001, ' num2str(units)]);
+end
+end
+rbfplot1(x,y,f,units);
