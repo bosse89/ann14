b1f94aa5fd7e7ee1091b363bd712a8520eff88b4
Sebastian Olsson <sebelino@hotmail.com>
2014-02-04 12:58:25 +0100

    Märkte ut punkter i approximationsplotten.

--- lab2/four_one_least_mean_sq.m
+++ lab2/four_one_least_mean_sq.m
@@ -1,21 +1,33 @@
-x = [0:0.1:2*pi]';%inputs
-f=sin(2*x);%desired outputs
-units=5;
-for units=56:56
-makerbf %output: m and var
-Phi = calcPhi(x,m,var);
-w=Phi\f;
-y=Phi*w;
-rvmax=max(abs(f-y));
-%display(rvmax);
-if rvmax<=0.1
-    display(['0.1, ' num2str(units)]);
-end
-if rvmax<=0.01
-    display(['0.01, ' num2str(units)]);
-end
-if rvmax<=0.001
-    display(['0.001, ' num2str(units)]);
-end
+x = [0:0.1:2*pi]'; % inputs
+f=sin(2*x); % desired outputs
+units=2;
+%for units=56:56
+for units=units:units
+    makerbf % output: m and var
+    Phi = calcPhi(x,m,var);
+    w=Phi\f;
+    y=Phi*w;
+    rvmax=max(abs(f-y));
+    %display(rvmax);
+    if rvmax<=0.1
+        display(['0.1, ' num2str(units)]);
+    end
+    if rvmax<=0.01
+        display(['0.01, ' num2str(units)]);
+    end
+    if rvmax<=0.001
+        display(['0.001, ' num2str(units)]);
+    end
 end
 rbfplot1(x,y,f,units);
+
+% Display points roughly corresponding to the approximation.
+xapprox = [x(1):(x(end)-x(1))/(units-1):x(end)]
+yapprox = []
+for xx=xapprox
+    [amin,argmin] = min(abs(x-xx))
+    yy = f(argmin)
+    yapprox = [yapprox yy]
+end
+subplot(2,1,1); hold on;
+plot(xapprox,yapprox,'r+');
