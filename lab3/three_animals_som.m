94665ade21852bc5d2766092237223993813398c
Andreas Cederholm <ance@konst-09.csc.kth.se>
2014-02-12 16:13:52 +0100

    Ännu mer uppg 3.

--- lab3/three_animals_som.m
+++ lab3/three_animals_som.m
@@ -5,16 +5,25 @@
 clear;
 animals;
 
+step = 0.2;
 epochs = 20;
-w = rand(100,84);
+neighbours = 50;
+fneighbours = neighbours;
+dneighbours = neighbours/epochs;
 
+w = rand(100,84);
 for epoch = 1:epochs
     for a = 1:32
         p = props(a,:);
         d = [];
         for i = 1:100
-            d(i) = (p-w(i,:)) * (p-w(i,:))';
+            d(i) = (p-w(i,:)) * (p-w(i,:))';% <== Correct???
         end
-        [x,i] = min(d);
+        [x,xi] = min(d);
+        for i = max(0,xi-neighbours):min(100,xi+neighbours)
+            
+        end
     end
+    fneighbours = fneighbours - dneighbours;
+    neighbours = floor(fneighbours);
 end
