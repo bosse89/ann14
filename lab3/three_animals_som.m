6245e663385a64378c1c6b0e1f2bb59dc78e9eea
Andreas Cederholm <ance@konst-09.csc.kth.se>
2014-02-12 15:47:09 +0100

    Lite mer uppg 3.

--- lab3/three_animals_som.m
+++ lab3/three_animals_som.m
@@ -1,9 +1,20 @@
+% 
+% 100 nodes, 84 attributes, 32 animals
+% 
+
+clear;
+animals;
 
 epochs = 20;
+w = rand(100,84);
 
 for epoch = 1:epochs
     for a = 1:32
         p = props(a,:);
-        p
+        d = [];
+        for i = 1:100
+            d(i) = (p-w(i,:)) * (p-w(i,:))';
+        end
+        [x,i] = min(d);
     end
 end
