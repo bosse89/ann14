2e9e63bef9222ef3c67e104452f9184a197dfbac
Andreas Cederholm <ance@konst-09.csc.kth.se>
2014-02-12 16:29:43 +0100

    uppg 3 klar?

--- lab3/three_animals_som.m
+++ lab3/three_animals_som.m
@@ -5,7 +5,7 @@
 clear;
 animals;
 
-step = 0.2;
+eta = 0.2;
 epochs = 20;
 neighbours = 50;
 fneighbours = neighbours;
@@ -20,10 +20,23 @@ for epoch = 1:epochs
             d(i) = (p-w(i,:)) * (p-w(i,:))';% <== Correct???
         end
         [x,xi] = min(d);
-        for i = max(0,xi-neighbours):min(100,xi+neighbours)
-            
+        for i = max(1,xi-neighbours):min(100,xi+neighbours)
+            w(i,:) = w(i,:) + eta*( p-w(i,:) );
         end
     end
     fneighbours = fneighbours - dneighbours;
     neighbours = floor(fneighbours);
 end
+
+pos = [];
+for a = 1:32
+    p = props(a,:);
+    d = [];
+    for i = 1:100
+        d(i) = (p-w(i,:)) * (p-w(i,:))';% <== Correct???
+    end
+    [x,xi] = min(d);
+    pos(a) = xi;
+end
+[dummy,order] = sort(pos);
+snames(order)'
