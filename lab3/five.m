8069a74f7587d8176a6e1852047dbb3b80284267
Sebastian Olsson <sebelino@hotmail.com>
2014-02-13 11:30:49 +0100

    Resten av skelettet för five.

--- lab3/five.m
+++ lab3/five.m
@@ -1,9 +1,5 @@
 clear
 votesscript;
-mpsexscript;
-mppartyscript;
-mpdistrictscript;
-
 % votes: [349] x [31] -> {0,0.5,1}
 % mpsex: [349] -> {0,1}
 % mpparty: [349] -> [7]
@@ -14,7 +10,8 @@ epochs = 20;
 neighbours = 50;
 fneighbours = neighbours;
 dneighbours = neighbours/epochs;
-m = 100; % # output nodes
+m = 10; % # rows
+n = 10; % # cols
 N = 31;  % # attributes (dimension)
 t = 349;  % # training samples
 
@@ -45,5 +42,17 @@ for a = 1:t
     [x,xi] = min(d);
     pos(a) = xi;
 end
-[dummy,order] = sort(pos);
-snames(order)'
+
+[x,y] = meshgrid([1:m],[1:n]);
+xpos = reshape(x,1,m*n)';
+ypos = reshape(y,1,m*n)';
+
+a = ones(1,100)*350';
+a(pos) = 1:349;
+a'
+
+%mpsexscript;
+mppartyscript;
+%mpdistrictscript;
+p = [mpparty;0];
+image(p(reshape(a,10,10))+1)
