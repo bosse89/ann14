781d67213761e241a0e133f16ecbaf07db8c5e3e
Sebastian Olsson <sebelino@hotmail.com>
2014-02-20 11:33:24 +0100

    Arbete med distances på 5.1.

--- lab4/lab4.m
+++ lab4/lab4.m
@@ -4,7 +4,7 @@ X = vm([0 0 1 0 1 0 0 1;0 0 0 0 0 1 0 0;0 1 1 0 0 1 0 1])
 %X = vm([0 0 1 0 1 0 0 1])
 N = size(X,2); % (?)
 P = size(X,1);
-W = X'*X
+W = X'*X;
 
 for i=1:N
     for j=1:N
@@ -14,21 +14,28 @@ for i=1:N
         end
     end
 end
-%Wij = Wij/N;
-Wij
+Wij = Wij/N;
 
 % W =/= Wij? Varför är det så?
 % W verkar vara den som funkar korrekt att uppdatera med.
 
 % Testpatterns:
-Xi = X;
-Xi(1,1) = 1; % En liten mutation för att se om det funkar ändå
-Xi
-updated = sgn(W*Xi')'
+Xd = X;
+Xd
+
+% Distortions
+distortion = [0 0 0 0 0 0 1 0]
+Xd = vm(xor(t0(X),distortion(ones(1,3),:)))
+
+% Distances
+distances = abs(sum(sign(Xd-X),2))
+
+% Apply update rule
+updated = sgn(W*Xd')'
 
 for mu=1:P
-    if isequal(X(mu,:),X(mu,:))
-        disp(['Xi',num2str(mu),' == X',num2str(mu)])
+    if isequal(Xd(mu,:),X(mu,:))
+        disp(['Xd',num2str(mu),' == X',num2str(mu)])
     end
     if isequal(updated(mu,:),X(mu,:))
         disp(['updated',num2str(mu),' == X',num2str(mu)])
