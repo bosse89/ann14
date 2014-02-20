ccc412d1c45256763c5c7c5cbc05d7cd1e3c0764
Sebastian Olsson <sebelino@hotmail.com>
2014-02-20 10:39:01 +0100

    Grundläggande update-rule.

--- lab4/lab4.m
+++ lab4/lab4.m
@@ -1,7 +1,8 @@
-X = vm([0 0 1 0 1 0 0 1;0 0 0 0 0 1 0 0;0 1 1 0 0 1 0 1]);
+%X = vm([0 0 1 0 1 0 0 1;0 0 0 0 0 1 0 0;0 1 1 0 0 1 0 1]);
+X = vm([0 0 1 0 1 0 0 1])
 N = size(X,2); % (?)
 P = size(X,1);
-W = X'*X;
+W = X'*X
 
 for i=1:N
     for j=1:N
@@ -11,4 +12,18 @@ for i=1:N
         end
     end
 end
-Wij = Wij/N
+%Wij = Wij/N;
+Wij
+
+test = X(1,:);
+test(1:5) = 1
+updated = sgn(Wij*test')'
+
+for mu=1:P
+    if isequal(test,X(mu,:))
+        disp(['test and X',num2str(mu),' are equal'])
+    end
+    if isequal(updated,X(mu,:))
+        disp(['updated and X',num2str(mu),' are equal'])
+    end
+end
